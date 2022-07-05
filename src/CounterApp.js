fa536296ca0535141c832d2037368927f4349dc1
vverac <viverace@gmail.com>
2022-07-05 16:39:34 -0400

    first commit

---
+++ src/CounterApp.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+const CounterApp = () => {
+
+  const [counter, setCounter] = useState(0)
+
+  const incrementCounter = () => {
+    setCounter(counter + 1)
+  }
+
+  return (
+    <div>
+      <button onClick={incrementCounter}>Incrementar</button>
+      <h1>Clicks: {counter}</h1>
+    </div>
+  )
+}
+
+export default CounterApp;
